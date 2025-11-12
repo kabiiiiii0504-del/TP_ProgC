a06773f078c59eb127bc3d74e9656339bcf7ecfc
kabiiiiii0504-del <kabiiiiii0504@gmail.com>
2025-11-12 14:59:51 +0100

    Update variables.c

--- TP1/src/variables.c
+++ TP1/src/variables.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-int main(void)
-{
+int main() {
+    // Déclaration et affectation des variables
     char c = 'A';
     signed char sc = -65;
     unsigned char uc = 200;
@@ -9,25 +9,41 @@ int main(void)
     short s = -12345;
     unsigned short us = 54321;
 
-    int i = -100000;
-    unsigned int ui = 3000000000U;
+    int i = -123456;
+    unsigned int ui = 123456;
 
-    long l = -2000000000L;
-    unsigned long ul = 4000000000UL;
+    long int li = -123456789L;
+    unsigned long int uli = 123456789UL;
 
-    long long ll = -9000000000000000000LL;
-    unsigned long long ull = 18000000000000000000ULL;
+    long long int lli = -9876543210LL;
+    unsigned long long int ulli = 9876543210ULL;
 
     float f = 3.14f;
-    double d = 2.71828;
-    long double ld = 1.61803L;
+    double d = 2.718281828;
+    long double ld = 1.6180339887L;
 
-    printf("char=%c  sc=%d  uc=%u\n", c, sc, uc);
-    printf("s=%d  us=%u\n", s, us);
-    printf("i=%d  ui=%u\n", i, ui);
-    printf("l=%ld  ul=%lu\n", l, ul);
-    printf("ll=%lld  ull=%llu\n", ll, ull);
-    printf("f=%.2f  d=%.5f  ld=%.5Lf\n", f, d, ld);
+    // Affichage des valeurs
+    printf("=== Variables de types de base ===\n\n");
+
+    printf("char : %c\n", c);
+    printf("signed char : %d\n", sc);
+    printf("unsigned char : %u\n", uc);
+
+    printf("short : %d\n", s);
+    printf("unsigned short : %u\n", us);
+
+    printf("int : %d\n", i);
+    printf("unsigned int : %u\n", ui);
+
+    printf("long int : %ld\n", li);
+    printf("unsigned long int : %lu\n", uli);
+
+    printf("long long int : %lld\n", lli);
+    printf("unsigned long long int : %llu\n", ulli);
+
+    printf("float : %f\n", f);
+    printf("double : %lf\n", d);
+    printf("long double : %Lf\n", ld);
 
     return 0;
 }
