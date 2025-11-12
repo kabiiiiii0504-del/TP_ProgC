60eb6a9e8b370e7fc8017791a04cdadca2c4e875
kabiiiiii0504-del <kabiiiiii0504@gmail.com>
2025-11-12 15:09:56 +0100

    Update calculs.c

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int num1 = 10, num2 = 3;
+    char op = '+';
+
+    switch (op) {
+        case '+': printf("%d + %d = %d\n", num1, num2, num1 + num2); break;
+        case '-': printf("%d - %d = %d\n", num1, num2, num1 - num2); break;
+        case '*': printf("%d * %d = %d\n", num1, num2, num1 * num2); break;
+        case '/': if (num2) printf("%d / %d = %d\n", num1, num2, num1 / num2);
+                  else printf("Division par zéro !\n"); break;
+        case '%': if (num2) printf("%d %% %d = %d\n", num1, num2, num1 % num2);
+                  else printf("Modulo par zéro !\n"); break;
+        case '&': printf("%d & %d = %d\n", num1, num2, num1 & num2); break;
+        case '|': printf("%d | %d = %d\n", num1, num2, num1 | num2); break;
+        case '~': printf("~%d = %d, ~%d = %d\n", num1, ~num1, num2, ~num2); break;
+        default:  printf("Opérateur inconnu !\n");
+    }
+
+    return 0;
+}
+
 
