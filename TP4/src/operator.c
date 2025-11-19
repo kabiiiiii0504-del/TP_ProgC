6e714ed4d7e7c81dee1350e9fe4f8d47dd3942cb
kabiiiiii0504-del <kabiiiiii0504@gmail.com>
2025-11-19 15:11:44 +0100

    Update operator.c

--- TP4/src/operator.c
+++ TP4/src/operator.c
@@ -1,14 +1,48 @@
-#ifndef OPERATOR_H
-#define OPERATOR_H
+#include "operator.h"
 
-// Déclaration des fonctions pour les opérations
-int somme(int num1, int num2);
-int difference(int num1, int num2);
-int produit(int num1, int num2);
-int quotient(int num1, int num2);
-int modulo(int num1, int num2);
-int et(int num1, int num2);    // AND bit-à-bit
-int ou(int num1, int num2);    // OR bit-à-bit
-int negation(int num1);        // ~ (bit-à-bit) sur le premier nombre
+// Addition
+int somme(int num1, int num2) {
+    return num1 + num2;
+}
 
-#endif
+// Soustraction
+int difference(int num1, int num2) {
+    return num1 - num2;
+}
+
+// Multiplication
+int produit(int num1, int num2) {
+    return num1 * num2;
+}
+
+// Division
+int quotient(int num1, int num2) {
+    if (num2 != 0)
+        return num1 / num2;
+    else
+        return 0; // valeur par défaut si division par zéro
+}
+
+// Modulo
+int modulo(int num1, int num2) {
+    if (num2 != 0)
+        return num1 % num2;
+    else
+        return 0; // valeur par défaut si division par zéro
+}
+
+// AND bit-à-bit
+int et(int num1, int num2) {
+    return num1 & num2;
+}
+
+// OR bit-à-bit
+int ou(int num1, int num2) {
+    return num1 | num2;
+}
+
+// Négation bit-à-bit du premier nombre
+int negation(int num1) {
+    return ~num1;
+}
+
