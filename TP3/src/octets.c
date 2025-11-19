b8a0b2e6cac2f30150333f4df1f802bd4d88cb2e
kabiiiiii0504-del <kabiiiiii0504@gmail.com>
2025-11-19 14:28:30 +0100

    Update octets.c

---
+++ TP3/src/octets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    // Types de base
+    printf("La taille de char       : %zu octets\n", sizeof(char));
+    printf("La taille de short      : %zu octets\n", sizeof(short));
+    printf("La taille de int        : %zu octets\n", sizeof(int));
+    printf("La taille de long       : %zu octets\n", sizeof(long));
+    printf("La taille de long long  : %zu octets\n", sizeof(long long));
+    printf("La taille de float      : %zu octets\n", sizeof(float));
+    printf("La taille de double     : %zu octets\n", sizeof(double));
+    printf("La taille de long double: %zu octets\n", sizeof(long double));
+
+    printf("\n");
+
+    // Pointeurs simples
+    printf("La taille de int*   : %zu octets\n", sizeof(int*));
+    printf("La taille de int**  : %zu octets\n", sizeof(int**));
+    printf("La taille de char*  : %zu octets\n", sizeof(char*));
+    printf("La taille de char** : %zu octets\n", sizeof(char**));
+    printf("La taille de char***: %zu octets\n", sizeof(char***));
+    printf("La taille de float* : %zu octets\n", sizeof(float*));
+    printf("La taille de float**: %zu octets\n", sizeof(float**));
+    printf("La taille de float***: %zu octets\n", sizeof(float***));
+
+    return 0;
+}
